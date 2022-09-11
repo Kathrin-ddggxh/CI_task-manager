aabd3cee7afe041e606730876a728f584b85204f
Kay-ddggxh <kathrin.welfare@gmail.com>
2022-09-11 10:03:46 +0000

    Create run.py

---
+++ run.py
@@ -0,0 +1,11 @@
+import os
+from taskmanager import app
+
+
+# tell app how and where to run application
+if __name__ == "__main__":
+    app.run(
+        host=os.environ.get("IP"),
+        port=int(os.environ.get("PORT")),
+        debug=os.environ.get("DEBUG")
+    )

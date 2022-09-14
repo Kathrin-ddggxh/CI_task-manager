1343335583f7e39bae4ec2606b743953d5ba273d
Kay-ddggxh <kathrin.welfare@gmail.com>
2022-09-14 13:46:24 +0000

    Create template and function to add task

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,5 +1,19 @@
-// Initialize collapsable navbar: Materialize script
+//  Materialize script
 document.addEventListener('DOMContentLoaded', function () {
+    // collapsable navbar initialization
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
+
+    // datepicker initialization
+    let datepicker = document.querySelectorAll(".datepicker");
+    M.Datepicker.init(datepicker, {
+        format: "dd mmmm, yyyy",
+        i18n: {
+            done: "Select"
+        }
+    });
+
+    // select initialization
+    let selects = document.querySelectorAll("select");
+    M.FormSelect.init(selects);
 });
